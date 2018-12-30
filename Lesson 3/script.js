31d67d6a143a46de5370f3fb61a3060547dacb2a
Ivan <jevgeniblinov@gmail.com>
2018-12-30 19:46:31 +0100

    First commit

---
+++ Lesson 3/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = false;
+
+null;
+var obj = {};
+
+
+var LeftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(4/0);
+console.log('string');
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[1]);
+
+//alert("Hello World!")
+
+/*let answer = confirm("Are you here?");
+console.log(answer)*/
+
+/*let answer = prompt("Are you here?", "YES");
+console.log(typeof(answer));*/
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+let isChecked = true,
+    isClose = true;
+
+    console.log(isChecked && isClose);
